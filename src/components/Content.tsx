a6bc1bd43e923c2a3dea27087df11ae459352253
hlaing-dev <hlaingmin1235145@gmail.com>
2025-02-08 11:32:00 +0700

    update

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -4,13 +4,14 @@ import { Link, useNavigate } from "react-router-dom";
 import { showToast } from "../pages/profile/error/ErrorSlice";
 import { setActiveNav } from "../pages/home/slice/HomeSlice";
 
-const Content = ({ notice }: any) => {
+const Content = ({ notice,handleAppClose }: any) => {
   const dispatch = useDispatch();
 
   const navigate = useNavigate();
   const [pageType, setPageType] = useState(false);
 
   const type = notice?.extend?.page_type;
+  console.log(type);
   // console.log('type', type)
 
   useEffect(() => {
@@ -34,6 +35,7 @@ const Content = ({ notice }: any) => {
       dispatch(setActiveNav(3));
 
       navigate("/explorer");
+      handleAppClose()
     } else {
       dispatch(
         showToast({
@@ -45,6 +47,8 @@ const Content = ({ notice }: any) => {
     }
   };
 
+  console.log(notice);
+
   return (
     <div className="content p-3">
       <div className="text-card">
@@ -54,7 +58,7 @@ const Content = ({ notice }: any) => {
         <p className="mt-3 text-[#888] text-[10px] font-[500]">
           {notice.content}
         </p>
-        {pageType ? (
+        {/* {pageType ? (
           <>
             {notice.extend.parameters?.video_id && (
               <button
@@ -95,6 +99,20 @@ const Content = ({ notice }: any) => {
               {notice.extend.page_name}
             </button>
           )
+        )} */}
+        {pageType ? (
+          <button className="noti-btn mt-6" onClick={() => JumpAction(notice)}>
+            {notice.extend.page_name}
+          </button>
+        ) : (
+          <a
+          target="_blink"
+            href={notice?.extend?.page_path}
+            className="noti-btn mt-6"
+            // onClick={() => JumpAction(notice)}
+          >
+            {notice.extend.page_name}
+          </a>
         )}
       </div>
     </div>
